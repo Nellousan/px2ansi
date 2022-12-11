d154fea2730a63a8da60afa047bffffbe0a9db29
Nell Fauveau <nellousan@hotmail.fr>
2022-12-11 20:05:21 +0100

    feat(px2ansi): added shebang and proper header

--- px2ansi.py
+++ px2ansi.py
@@ -1,3 +1,19 @@
+#!/usr/bin/python3
+# Px2ANSI: Pixel Art to ANSI Art conversions
+# Author: Nell Fauveau
+# https://github.com/Nellousan/px2ansi
+#
+# Copyright 2022 Nell Fauveau
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# version 2 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
 from PIL import Image
 import numpy as np
 import argparse
